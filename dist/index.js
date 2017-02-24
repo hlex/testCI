8bf9980e27be84ed5324cca089af20fa1d9441f6
Mondit Thumniramon <mondit.thum@gmail.com>
2017-02-24 18:17:15 +0700

    feat(change repository structure): add npm run build

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var add = function add(x, y) {
+  return x + y;
+};
+
+var minus = function minus(x, y) {
+  return x - y;
+};
+
+var multiply = function multiply(x, y) {
+  return x * y;
+};
+
+exports.add = add;
+exports.minus = minus;
